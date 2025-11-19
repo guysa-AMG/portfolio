fcf722f6f925b0965bc4ae1fbcb0241c7671ad8f
gumumjhb025 <gumumjhb025@student.wethinkcode.co.za>
2025-11-19 16:52:36 +0200

    managed to fix the badge Conveyor and added some personal porjects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,8 @@ export default function Home() {
   return (
 
     <div>
-       <section className="landingPage"> <div className="lines">
+       <section className="landingPage">
+         <div className="lines">
   <div className="line"></div>
   <div className="line"></div> 
   <div className="line"></div>
@@ -44,10 +45,28 @@ export default function Home() {
       <Button size="medium" variant="contained" onClick={()=>scrolldn()}>whoami</Button>
        </section>
     <section >
-      <h2>Projects</h2>
+      <h2>Public Projects</h2>
+      <ul className="ProjView">
+        <li><h3><a className="projLink" href="https://scentjozi-466018.uk.r.appspot.com/">ScentJozi</a></h3>
+        <p>Scent Jozi is a demo ecommerce website built with react+nexjs with payfast intergrate as payment gateway. and firebase and supabase as third party used for storage and Authentication.   </p></li>
+        
+        <li><h3> <a className="projLink" href="https://frapapi-47403969124.europe-west1.run.app/">test Fragrance Api</a></h3>
+        a Dockerized google cloud build + cloud run deployed free api which consists 20,000 fragrances useful for simple test when developing ecommerce platform web or mobile . 
+        </li>
+         <li><h3><a className="projLink" href="https://peecha-kutcha.vercel.app">Peecha Kutcha</a></h3>
+        <p>Peecha Kutcha is a simple webapp which helps people practise presentation based on random images (related to the topic given)   </p></li>
+        <li><h3><a className="projLink" href="https://siyakhanya.vercel.app/">SiyaKhanya</a></h3>
+        <p>Siyakhanya was developed as part of the numisa hackathon whereby the problem it was trying to reslove was of "Electricity and inability for SA Community to move to Alternative Energy" what we do is we introduce we introduce grouped community with affordable suppliers (Market Place)  </p></li>
+       
+        
+      </ul>
+    </section>
+    <section>
+      <h2>Participation and contribution</h2>
       <ul>
-        <li>ScentJozi</li>
-        <li>test Fragrance Api</li>
+        <li></li>
+        <li></li>
+        <li></li>
       </ul>
     </section>
     <section>
