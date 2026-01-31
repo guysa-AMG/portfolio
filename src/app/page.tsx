8765d671063ecb2bc0b311c49d1fa177d22b1e93
gumumjhb025 <moosathejoker@gmail.com>
2026-01-31 22:00:03 +0200

    changes to the landing pahe and updated project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,16 +20,16 @@ export default function Home() {
   return (
 
     <div>
+        
        <section className="landingPage">
-         <div className="lines">
-  <div className="line"></div>
-  <div className="line"></div> 
-  <div className="line"></div>
-  <div className="line"></div>
-  <div className="line"></div>
-</div>
+       <div className="videoanim">
+       <video src={"/back.mp4"} autoPlay loop/>
+       <video  src={"/back.mp4"} autoPlay loop/></div>
+       
 
-       <h4 className="m-0 p-0">Hi there i'm</h4>
+      <div className="contentBd">
+        <div>
+       <h4 className="m-0 p-0 ">Hi there i'm</h4>
        
        <h1 className="type">
         <TypingText
@@ -42,31 +42,46 @@ export default function Home() {
         />
         </h1>
     
-      <Button size="medium" variant="contained" onClick={()=>scrolldn()}>whoami</Button>
+      <Button size="medium" variant="contained" onClick={()=>scrolldn()}>whoami</Button></div>
+    </div>
        </section>
-    <section >
+    <section className="proj" >
       <h2>Public Projects</h2>
       <ul className="ProjView">
-         <li><h3><a className="projLink" href="https://scentjozi-466018.uk.r.appspot.com/">Vicuna med</a></h3>
+         <li>
+          
+          <h3><a className="projLink" href="https://scentjozi-466018.uk.r.appspot.com/">Vicuna med</a></h3>
+          <div className="proj-creation"> created: 2025/11/26</div>
         <p>Vicuna med is a flutter application built to serve the purpose of LLM reading and explaining medical note to patient without the compromise of privacy currently on Beta trial in playstore  </p></li>
         
 
         <li><h3><a className="projLink" href="https://scentjozi-466018.uk.r.appspot.com/">ScentJozi</a></h3>
+        <div className="proj-creation"> created: 2025/11/26</div>
         <p>Scent Jozi is a demo ecommerce website built with react+nexjs with payfast intergrate as payment gateway. and firebase and supabase as third party used for storage and Authentication.   </p></li>
         
-        <li><h3> <a className="projLink" href="https://frapapi-47403969124.europe-west1.run.app/">test Fragrance Api</a></h3>
-        a Dockerized google cloud build + cloud run deployed free api which consists 20,000 fragrances useful for simple test when developing ecommerce platform web or mobile . 
+
+        <li><h3> <a className="projLink" href="https://frapapi-47403969124.europe-west1.run.app/">test Fragrance Api</a></h3><p>  
+        a Dockerized google cloud build + cloud run deployed free api which consists 20,000 fragrances useful for simple test when developing ecommerce platform web or mobile . </p>
         </li>
          <li><h3><a className="projLink" href="https://peecha-kutcha.vercel.app">Peecha Kutcha</a></h3>
+       <div className="proj-creation"> created: 2025/08/04</div>
         <p>Peecha Kutcha is a simple webapp which helps people practise presentation based on random images (related to the topic given)   </p></li>
+        
         <li><h3><a className="projLink" href="https://siyakhanya.vercel.app/">SiyaKhanya</a></h3>
+        <div className="proj-creation"> created: 2025/11/24</div>
         <p>Siyakhanya was developed as part of the numisa hackathon whereby the problem it was trying to reslove was of "Electricity and inability for SA Community to move to Alternative Energy" what we do is we introduce we introduce grouped community with affordable suppliers (Market Place)  </p></li>
        
+          <li><h3><a className="projLink" href="https://github.com/guysa-AMG/JVExpress">JVExpress</a></h3>
+        <div className="proj-creation"> created: 2026/01/05</div>
+        <p>J-Express is a high-performance, lightweight Java web framework built from the raw socket level up. By bypassing heavy enterprise abstractions, it brings the minimalist, "plug-and-play" philosophy of Node.js Express to the Java ecosystem.
+
+Designed for developers who value transparency and speed</p></li>
         
       </ul>
     </section>
     
     <section>
+      
       <h2>Participation and contribution</h2>
       <ul>
         <li></li>
@@ -83,9 +98,7 @@ export default function Home() {
 
 
     </section>
-    <section>
-      Contacts
-    </section>
+    
     </div>
     
   );
