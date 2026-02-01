dc0398dadf933b641adbaf19f7165e3dcd4d36dd
gumumjhb025 <moosathejoker@gmail.com>
2026-02-01 21:42:48 +0200

    added some data under contribution

---
+++ src/components/ghcontrib.jsx
@@ -0,0 +1,15 @@
+
+
+
+export default function GitHubContrib(){
+
+
+
+    return (
+
+        <div >
+        
+<img src="https://github-readme-stats.vercel.app/api?username=guysa-AMG&show_icons=true&theme=radical" alt="GitHub Stats" />
+      </div>
+    );
+}
