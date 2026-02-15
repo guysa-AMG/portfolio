b1513ac6c0e83172e5dc78bc45ec31f83208f273
Guysa <63824183+guysa-AMG@users.noreply.github.com>
2026-02-16 00:00:17 +0200

    Update frame.jsx

--- src/app/frame.jsx
+++ src/app/frame.jsx
@@ -25,6 +25,10 @@ export default function Frame({children}){
                 <a href="https://www.reddit.com/user/Agreeable_Rough_2563/?utm_source=share&utm_medium=web3x&utm_name=web3xcss&utm_term=1&utm_content=share_button">
                 <Reddit sx={{"fontSize":"30px"}}/>
                 </a>
+                 <a href="https://stackoverflow.com/users/29358956/moosa-ahmed-mume">
+                <Reddit sx={{"fontSize":"30px"}}/>
+                </a>
+                
                 
                 </li>
         </ul>
